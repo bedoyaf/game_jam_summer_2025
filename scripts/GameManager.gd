684c960bcfa0c21144027579ebc93362d82052c2
rolloj <jakubrollo10@gmail.com>
2025-03-28 20:04:29 +0100

    again tralalelo

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,11 +1,44 @@
 extends Node
 
+var balance : float = 100.0
+var charisma : float = 50.0
+var oxygen : float = 100.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var balance_threshold : float = 0.0
+var oxygen_threshold : float = 0.0
 
+signal game_over
+signal update_ui
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func adjust_balance(amount : float):
+	balance += amount
+	#if balance <= balance_threshold:
+		#_trigger_game_over("Rovnováha ztracena!")
+	#emit_signal("update_ui", "balance", balance)
+
+func adjust_charisma(amount : float):
+	charisma += amount
+	#emit_signal("update_ui", "charisma", charisma)
+
+func adjust_oxygen(amount : float):
+	oxygen += amount
+	#if oxygen <= oxygen_threshold:
+	#    _trigger_game_over("Kyslík vyčerpán!")
+	#emit_signal("update_ui", "oxygen", oxygen)
+
+func _trigger_game_over(reason : String):
+	print("Game Over: " + reason)
+	#emit_signal("game_over", reason)
+
+func reset_stats():
+	balance = 100.0
+	charisma = 50.0
+	oxygen = 100.0
+	#emit_signal("update_ui", "balance", balance)
+	#emit_signal("update_ui", "charisma", charisma)
+	#emit_signal("update_ui", "oxygen", oxygen)
+
+func _process(delta):
+
+	if oxygen > 0:
+		adjust_oxygen(-0.1 * delta)  
