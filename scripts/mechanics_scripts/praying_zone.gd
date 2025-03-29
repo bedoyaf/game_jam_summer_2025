af153693aaa985b2ad6ec69261e1056315ec7e35
martin-18246 <martin.mikestik@gmail.com>
2025-03-29 02:18:34 +0100

    add praying zone

--- scripts/mechanics_scripts/praying_zone.gd
+++ scripts/mechanics_scripts/praying_zone.gd
@@ -5,12 +5,12 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		print("ENTER playing zone.")
+		print("ENTER praying zone.")
 		body.enter_praying_zone()
 
 
 
 func _on_body_exited(body):
 	if body.is_in_group("Player"):
-		print("EXIT playing zone.")
+		print("EXIT praying zone.")
 		body.exit_praying_zone()
