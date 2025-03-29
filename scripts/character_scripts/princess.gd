355b3ad943bf0ed1d60b421638cd952f13f30bb5
martin-18246 <martin.mikestik@gmail.com>
2025-03-29 11:12:15 +0100

    normalize

--- scripts/character_scripts/princess.gd
+++ scripts/character_scripts/princess.gd
@@ -10,9 +10,6 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 
 	move_and_slide()
